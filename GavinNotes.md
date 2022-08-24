9d4b5c8d870a2acad148bcead3f5f081e70dfa77
diakt <igs2107@columbia.edu>
2022-08-24 10:18:11 -0400

    expletive django

---
+++ GavinNotes.md
@@ -0,0 +1,6 @@
+Pycache is evil, like bad cockroaches
+as_view no longer necessary
+It hates the about template being referenced in base
+the CSS is 404, think it is a static pointing issue
+
+don't think about is valid, getting an about not found, not valid view function/pattern name for no reverse match reasons
--- main_app/urls.py
+++ main_app/urls.py
@@ -3,21 +3,22 @@ from . import views
 
 #create routes for patient portal, maps to views.py
 urlpatterns=[
-    path('', views.Home.as_view(), name='home'), 
-    path('patient_request/',views.Patient_Request.as_view(), name='patient_request'), 
-    path('patient/<int:patientID>/detail/', views.Patient_details.as_view(), name='patient_detail'),
+    path('', views.Home, name='home'), 
+    path('patient_request/',views.Patient_Request, name='patient_request'), 
+    path('patient/<int:patientID>/detail/', views.Patient_Details, name='patient_detail'),
     path('accounts/signup/', views.Signup.as_view(), name="signup"),
+    path('about/', views.About, name="about"),
    
 
 #create routes for Scheduler/Admin portal
-    path('admin/', views.Home_Admin.as_views(), name='admin'),
-    path('admin/patient_request/', views.Admin_Patient_Request.as_view(), name='admin_patient_request'),
-    path('admin/patient_list/', views.Admin_Patient_list.as_view(), name='admin_patient_list'),
+    # path('admin/', views.Home_Admin, name='admin'),
+    # path('admin/patient_request/', views.Admin_Patient_Request.as_view(), name='admin_patient_request'),
+    # path('admin/patient_list/', views.Admin_Patient_list.as_view(), name='admin_patient_list'),
 
 #create routes for Provider portal
-    path('provider/', views.Home_Provider.as_view(), name='provider'),
-    path('provider/schedule', views.Schedule.as_view(), name='schedule'),
-    path('provider/client_list', views.Provider_client_list.as_views(), name='client_list')
+    # path('provider/', views.Home_Provider.as_view(), name='provider'),
+    # path('provider/schedule', views.Schedule.as_view(), name='schedule'),
+    # path('provider/client_list', views.Provider_client_list.as_views(), name='client_list')
 
 
     
